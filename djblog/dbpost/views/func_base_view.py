e5de86ed5216c8cc2bcaab0382c147c69a0a8dff
biswasbijoy <biswasbijoy414@gmail.com>
2023-08-27 01:39:45 +0600

    csrf token problem solved

--- djblog/dbpost/views/func_base_view.py
+++ djblog/dbpost/views/func_base_view.py
@@ -23,7 +23,7 @@ def post_create_view(request, *args, **kwargs) :
         if form.is_valid() :
             cl_data = form.cleaned_data
             post_obj = Post.objects.create(title=cl_data["title"], user_id=cl_data["user_id"])
-            # return redirect("dbpost:post_details", post_obj.id)
+            return redirect("dbpost:post_details", post_obj.id)
             
     return render(request, "dbpost/post_create.html", {"form": form})
 
@@ -31,11 +31,11 @@ def post_create_view(request, *args, **kwargs) :
 
 
 # def post_create_view(request, *args, **kwargs) :
-    form  = PostCreateForm()
-    if request.method == "POST" :
-        data = request.POST
-        form = PostCreateForm(data=data)
-        if form.is_valid() :
-            form.save()
-            return redirect("dbpost:post_details", form.instance.id)
-    return render(request, "dbpost/post_create.html", {"form": form})
+    # form  = PostCreateForm()
+    # if request.method == "POST" :
+    #     data = request.POST
+    #     form = PostCreateForm(data=data)
+    #     if form.is_valid() :
+    #         form.save()
+    #         return redirect("dbpost:post_details", form.instance.id)
+    # return render(request, "dbpost/post_create.html", {"form": form})
--- djblog/djblog/settings.py
+++ djblog/djblog/settings.py
@@ -134,3 +134,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'dbauth.User'
+
+
+CSRF_TRUSTED_ORIGINS=[]
